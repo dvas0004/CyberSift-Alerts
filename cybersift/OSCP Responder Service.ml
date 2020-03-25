c9b441b42cc464c1dad33d80bed1e1bcc75923da
tmus-Cybersift <61494934+tmus-Cybersift@users.noreply.github.com>
2020-03-25 13:10:23 +0100

    #15

---
+++ cybersift/OSCP Responder Service.ml
@@ -0,0 +1,38 @@
+# OSCP Responder Service
+### Description
+
+A security setting was updated on the OCSP Responder Service 
+
+-------------------
+### Severity
+
+High
+
+-------------------
+<!---### Detailed Information
+
+- Why is this alert triggered?
+- What are the typical causes that generate this alert? (e.g. port scans, unusual file access activity, etc...)
+- Which corroborating information should be looked up?
+- Any supporting queries to get more information?
+- Any supporting visualizations to get more information?
+
+-------------------
+### Possible causes of false positives
+
+=> What could cause this alert to mistakenly get generated? For example: <=
+- Joining a PC to a domain
+- Large file upload
+- Shared infrastructure (CDN)
+--->
+-------------------
+### References
+
+- https://docs.microsoft.com/en-us/windows-server/identity/ad-ds/plan/appendix-l--events-to-monitor
+
+-------------------
+### Supported Log Sources
+
+- windows security
+
+-------------------
