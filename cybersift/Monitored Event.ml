6a15e71731be35fbc5f0b2a1d5cc4be8eac9460e
tmus-Cybersift <61494934+tmus-Cybersift@users.noreply.github.com>
2020-03-25 12:53:07 +0100

    #15

---
+++ cybersift/Monitored Event.ml
@@ -0,0 +1,38 @@
+# Monitored Security Event Pattern
+### Description
+
+A monitored security event pattern has occurred. 
+-------------------
+### Severity
+
+High
+
+-------------------
+<!---### Detailed Information
+
+- Why is this alert triggered?
+- What are the typical causes that generate this alert? (e.g. port scans, unusual file access activity, etc...)
+- Which corroborating information should be looked up?
+- Any supporting queries to get more information?
+- Any supporting visualizations to get more information?
+
+-------------------
+### Possible causes of false positives
+
+=> What could cause this alert to mistakenly get generated? For example: <=
+- Joining a PC to a domain
+- Large file upload
+- Shared infrastructure (CDN)-->
+
+-------------------
+### References
+
+- https://docs.microsoft.com/en-us/windows-server/identity/ad-ds/plan/appendix-l--events-to-monitor 
+- https://docs.microsoft.com/en-us/windows/security/threat-protection/auditing/event-4618#security-monitoring-recommendations
+    
+-------------------
+### Supported Log Sources
+
+- windows security
+
+-------------------
